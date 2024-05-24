c8e9976a6ef8ca5abb4cb7c6d565ef14540df0bf
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-05-23 23:17:32 -0400

    Landing page

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=" flex-col items-center text-white md:px-0">
-      <nav className="w-full p-5 bg-fuchsia-950/70">
+    
+    <main className=" flex-col items-center md:px-0">
+
+      <nav className="w-full p-7 bg-sage">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white text-lg font-bold">
+          <div className="text-offwhite text-2xl font-extrabold -ml-24">
             <a href="/" className="flex items-center">
               <img
                   src="/favicon.ico"
@@ -15,20 +17,35 @@ export default function Home() {
               Bouquet.gen
             </a>
           </div>
-          <div className="space-x-4">
-            <a href="/dashboard" className="text-white hover:text-gray-300">Home</a>
-            <a href="/about" className="text-white hover:text-gray-300">About</a>
-            <a href="/contact" className="text-white hover:text-gray-300">Contact</a>
+          <div className="space-x-4 text-offwhite font-semibold text-xl -mr-20">
+            <a href="/dashboard" className="hover:text-white">Home</a>
+            <a href="/about" className="hover:text-white">About</a>
+            <a href="/contact" className="hover:text-white">Contact</a>
+            <a href="/signin" className="ml-2 bg-cream hover:bg-lightcream text-tahit text-xl font-bold py-3 px-6 rounded-md mt-4">
+            Sign in →
+            </a>
           </div>
         </div>
       </nav>
 
       <section className="relative flex-1 w-full bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url('/flowbackg.jpeg')`, height: "calc(100vh - 70px)"}}>
-      <div className="absolute inset-0 flex flex-col justify-center items-center">
-        <p className="text-4xl  font-bold z-10">
-          Generate thousands of bouquets with flowers related <br />to your prompt in both meaning and aesthetic.
-        </p>
-        <div className="absolute inset-0  backdrop-blur bg-black/70"></div>
+      <div className="absolute inset-0 backdrop-blur bg-black/80"></div>
+      <div className="absolute flex flex-row justify-start items-center left-32 top-1/2 space-x-96">
+        <div className="-mt-96 space-y-64">
+          <p className="text-6xl text-offwhite -mt-156 font-bold z-10 transform -translate-y-1/3">
+          🌺Generate thousands <br/> of bouquets rich in both <br/>meaning and aesthetic <br/>with the power of AI.🌼
+          </p>
+          <a href="/dashboard" className="ml-2 bg-cream hover:bg-lightcream text-tahit text-2xl font-bold py-5 px-10 rounded-md mt-4">
+            Get Started →
+          </a>
+
+        </div>
+
+        <img
+          src="/holdbouquet.jpg"
+          className="object-cover h-80 w-96 transform scale-150 rounded-xl transform -translate-y-1/2 border-4 border-offwhite p-2"
+        />
+
       </div>
     </section>
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'sage': '#667b68',
+        'lightsage': '#a3b899',
+        'richrose': '#783b5a',
+        'lightrose': '#aa6a6a',
+        'cream': '#f8d3c5',
+        'lightcream': '#f8d7ca',
+        'tahit': '#383139',
+        'offwhite': '#fceee9',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    
   },
   plugins: [],
 };
