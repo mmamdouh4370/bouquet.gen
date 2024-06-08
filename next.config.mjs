7198801c91f41cf53d3c39d014a59cd2fbf65321
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-06-08 02:03:34 -0400

    sd

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,3 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-        {
-            source: '/api/:path*',
-            destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
-        },
-        ]
-    },
 };
-
-module.exports = nextConfig
