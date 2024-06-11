c6e98a3e826b74ae8fdd1fcd790078096a2e3024
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-06-10 22:20:12 -0400

    Update Plus.js

--- src/components/ui/Plus.js
+++ src/components/ui/Plus.js
@@ -2,7 +2,7 @@ const Plus = () => {
     return (
       <div className="flex items-center justify-center w-12 h-12 border-2 border-current rounded-md p-2 bg-lightcream">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          <path stroke linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
       </div>
     )
