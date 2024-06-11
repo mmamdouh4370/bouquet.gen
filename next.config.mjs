147bb6de8f5a78b712256fe2f89bb98dd4652078
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-06-10 21:53:02 -0400

    Try and fix backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => {
-      return [
-        {
-          source: '/src/backend/:path*',
-          destination:
-            process.env.NODE_ENV === 'development'
-              ? 'http://127.0.0.1:3001/api/:path*'
-              : '/api/',
-        },
-      ]
-    },
-  }
-  
-export default nextConfig
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:3001/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
