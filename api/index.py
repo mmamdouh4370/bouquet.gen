d26862f55d5bc02e5902c586bec422d649415bb4
Mohammed Mamdouh <59239026+mmamdouh4370@users.noreply.github.com>
2024-06-24 21:22:53 -0400

    try serverless

--- api/index.py
+++ api/index.py
@@ -177,5 +177,4 @@ def unSaveBouquet():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="127.0.0.1", port=3001)
+    app.run()
